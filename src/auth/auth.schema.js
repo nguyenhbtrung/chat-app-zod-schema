a9ef2c792af1af0532b1aec50b4a9f00611cb2d7
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2025-07-30 08:52:00 +0700

    add auth schemas

---
+++ src/auth/auth.schema.js
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+
+export const registerSchema = z.object({
+    userName: z.string().min(1, { message: 'Username is required' }),
+    email: z.email({ message: 'Email format is invalid' }),
+    password: z.string().min(8, { message: 'Password must be at least 8 characters' })
+});
+
+export const loginSchema = z.object({
+    userName: z.string().min(1, { message: 'Username is required' }),
+    password: z.string().min(1, { message: 'Username is required' })
+});
