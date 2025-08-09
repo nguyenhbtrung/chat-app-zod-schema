843e2db60db3420f78e98768e49cddbd47ea0dd8
nguyenhbtrung <nguyenhbtrung1907@gmail.com>
2025-08-09 13:03:42 +0700

    add updateFriendshipSchema

--- src/friendship/friendship.schema.js
+++ src/friendship/friendship.schema.js
@@ -1,7 +1,15 @@
 import { z } from 'zod';
 
+const FRIENDSHIP_STATUS = ['pending', 'accepted', 'rejected', 'blocked', 'cancelled'];
+
 export const addFriendSchema = z.object({
     toUserId: z.coerce.number().int().positive({
         message: 'toUserId must be a positive integer',
+    }),
+});
+
+export const updateFriendshipSchema = z.object({
+    status: z.enum(FRIENDSHIP_STATUS, {
+        message: `status is not valid`,
     }),
 });
